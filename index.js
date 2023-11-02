414e17bd7e4299ae500d0cdc0c42ab724519bf88
MattNotarangelo <matt.notarangelo1@gmail.com>
2023-11-02 15:39:52 +1100

    a

--- index.js
+++ index.js
@@ -18,91 +18,88 @@ try {
   console.log(github.context.eventName);
   console.log(github.context.payload);
 
-  if (github.context.eventName !== "pull_request") {
-    console.log("received" + github.context.eventName);
-    return;
-  }
-
-  const message = {
-    cardsV2: [
-      {
-        cardId: "unique-card-id",
-        card: {
-          header: {
-            title: "New PR to review",
-            subtitle: "@" + payload.context.payload.owner.login,
-            imageUrl: "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
-            imageType: "CIRCLE",
-            imageAltText: "Avatar",
-          },
-          sections: [
-            {
-              header: "Pull Request Details",
-              collapsible: false,
-              uncollapsibleWidgetsCount: 1,
-              widgets: [
-                {
-                  decoratedText: {
-                    startIcon: {
-                      knownIcon: "DESCRIPTION",
+  if (github.context.eventName === "pull_request") {
+    const message = {
+      cardsV2: [
+        {
+          cardId: "unique-card-id",
+          card: {
+            header: {
+              title: "New PR to review",
+              subtitle: "@" + payload.context.payload.owner.login,
+              imageUrl: "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+              imageType: "CIRCLE",
+              imageAltText: "Avatar",
+            },
+            sections: [
+              {
+                header: "Pull Request Details",
+                collapsible: false,
+                uncollapsibleWidgetsCount: 1,
+                widgets: [
+                  {
+                    decoratedText: {
+                      startIcon: {
+                        knownIcon: "DESCRIPTION",
+                      },
+                      text: github.context.payload.title,
                     },
-                    text: github.context.payload.title,
                   },
-                },
-                {
-                  decoratedText: {
-                    icon: {
-                      iconUrl: "https://cdn0.iconfinder.com/data/icons/octicons/1024/repo-512.png",
+                  {
+                    decoratedText: {
+                      icon: {
+                        iconUrl: "https://cdn0.iconfinder.com/data/icons/octicons/1024/repo-512.png",
+                      },
+                      text: github.context.payload.repository,
                     },
-                    text: github.context.payload.repository,
                   },
-                },
-                {
-                  decoratedText: {
-                    icon: {
-                      iconUrl: "https://cdn0.iconfinder.com/data/icons/octicons/1024/repo-forked-512.png",
+                  {
+                    decoratedText: {
+                      icon: {
+                        iconUrl: "https://cdn0.iconfinder.com/data/icons/octicons/1024/repo-forked-512.png",
+                      },
+                      text: github.context.payload.head.ref,
                     },
-                    text: github.context.payload.head.ref,
                   },
-                },
-                {
-                  buttonList: {
-                    buttons: [
-                      {
-                        text: "View on Github",
-                        onClick: {
-                          openLink: {
-                            url: github.context.payload.url,
+                  {
+                    buttonList: {
+                      buttons: [
+                        {
+                          text: "View on Github",
+                          onClick: {
+                            openLink: {
+                              url: github.context.payload.url,
+                            },
                           },
                         },
-                      },
-                    ],
+                      ],
+                    },
                   },
-                },
-              ],
-            },
-          ],
+                ],
+              },
+            ],
+          },
         },
-      },
-    ],
-  };
+      ],
+    };
 
-  fetch(webhookUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-    body: JSON.stringify(message),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      console.log("Message sent successfully!");
+    fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(message),
     })
-    .catch((error) => {
-      console.error("Error sending message:", error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log("Message sent successfully!");
+      })
+      .catch((error) => {
+        console.error("Error sending message:", error);
+      });
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
