e75e6aeb9ba55ee2690836ae707d19f40ee15339
amogh-rokt <amogh.shetty@rokt.com>
2023-11-02 19:57:51 +1100

    3

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ try {
 
   const threadKey = github.context.payload.repository.name + github.context.payload.number;
   console.log(threadKey);
+  console.log(github.context);
 
   if (github.context.eventName === "pull_request") {
     const context = github.context.payload.pull_request;
