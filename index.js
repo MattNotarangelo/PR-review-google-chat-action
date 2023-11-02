67b5f2d1c610789409dccc24c5e234864dea6f12
MattNotarangelo <matt.notarangelo1@gmail.com>
2023-11-02 18:17:09 +1100

    s

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function post(webhookUrl, message) {
     body: JSON.stringify(message),
   });
 
-  console.log(response.json().then((j) => j.body));
+  response.json().then((j) => console.log(j.body));
 }
 
 try {
