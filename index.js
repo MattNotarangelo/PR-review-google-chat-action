851f919fe36eb618d4bfe626ea29b160b1a737ef
MattNotarangelo <matt.notarangelo1@gmail.com>
2023-11-02 13:22:59 +1100

    test

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  // // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput("who-to-greet");
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = new Date().toTimeString();
+  // core.setOutput("time", time);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
 
   const webhookUrl =
     "https://chat.googleapis.com/v1/spaces/AAAAxoPtGWE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=yHxt_zQVLby4cQ4nHxbhrM_RkIUB-GCDMLpETYHWfNk";
