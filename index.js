db9c9805f0a160512655e6ba056336da5b1a4256
MattNotarangelo <matt.notarangelo1@gmail.com>
2023-11-02 13:21:25 +1100

    test message

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const webhookUrl =
+    "https://chat.googleapis.com/v1/spaces/AAAAxoPtGWE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=yHxt_zQVLby4cQ4nHxbhrM_RkIUB-GCDMLpETYHWfNk";
+
+  const message = "hello world";
+
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(message),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log("Message sent successfully!");
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
